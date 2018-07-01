af13398a7ed2391987cfe2a0c3daf44cf9edd4af
Alyson Takai <shtakai@gmail.com>
2018-07-01 20:10:51 +0700

    Add accept named parameter

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Hell fuck lask'
+
+
+@app.route('/hell/<name>')
+def hell_there(name):
+    from datetime import datetime
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    html_content = "<html><head><title>Hell, fucklask</title></head><body>"
+    html_content += "<strong>Hell there, " + name + "!</strong>. It's "
+    html_content += formatted_now
+    html_content += "</body></html>"
+
+    return html_content
