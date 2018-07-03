6e7f2a5a40aa2205071eb581e27a0e6a6a9fd6a3
Alyson Takai <shtakai@gmail.com>
2018-07-03 20:38:05 +0700

    Add template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hell No, YES!"
+    user = {'username': 'shigel'}
+    return render_template('index.html', title='Home', user=user)
