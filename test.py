9ca9d4c50ba5d70c96ca03ec6ed922a9c4cf148d
Alyson Takai <shtakai@gmail.com>
2018-07-03 20:13:25 +0700

    Create simple /

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from app import app
+from microblog import app
 from flask_testing import TestCase
 import unittest
+
 
 class BaseTestCase(TestCase):
     def create_app(self):
@@ -8,6 +9,12 @@ class BaseTestCase(TestCase):
 
     def test_simple(self):
         self.assertEqual(200, 200)
+
+    def test_render(self):
+        response = self.client.get('/', content_type='html/txt')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hell No, YES!', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
