0e8c9e5a4718d7f0bd6b7dcec3f1a07a1d64e67a
Alyson Takai <shtakai@gmail.com>
2018-07-04 21:00:31 +0700

    Add Flask-SQLAlchemy Configuration

--- config.py
+++ config.py
@@ -2,8 +2,13 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
