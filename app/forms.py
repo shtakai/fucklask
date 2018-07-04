e860eddfeb6cbeb20ba8e9178d126286a3e40fb7
Alyson Takai <shtakai@gmail.com>
2018-07-04 20:16:07 +0700

    Fixed import location

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms import DataRequired
+from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
