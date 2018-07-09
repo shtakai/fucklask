2875625d46cc6a98dc9fcfcf1f092130911043eb
Alyson Takai <shtakai@gmail.com>
2018-07-09 19:56:53 +0700

    Add methods for password

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 
@@ -11,6 +12,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
