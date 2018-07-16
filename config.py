9513eacef8208ad4a0fce9a62b9ffc55f5b37c9e
Alyson Takai <shtakai@gmail.com>
2018-07-16 22:16:37 +0700

    Add default secret by secrets.token_urlsafe()

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import secrets
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -8,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_urlsafe()
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
