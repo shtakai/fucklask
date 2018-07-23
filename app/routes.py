c8c73671b0e7313a63192d162bdcd70772309170
Alyson Takai <shtakai@gmail.com>
2018-07-23 21:12:15 +0700

    Add user profile page

--- app/routes.py
+++ app/routes.py
@@ -64,3 +64,14 @@ def register():
         flash('Congraturations, you are now a fucked up user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
