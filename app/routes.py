a91760d01a720c135f50f68dea972a2304e43ffe
Alyson Takai <shtakai@gmail.com>
2018-07-23 21:50:48 +0700

    Add record last_seen

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, current_user, logout_user, login_required
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
